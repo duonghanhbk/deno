e10f062cbd2f19d67906d5102c3d0a06c6b36ae0
Nguyen Duong Hanh <hanhnd6@onemount.com>
2020-09-29 21:05:46 +0700

    authorized

--- controller/useController.ts
+++ controller/useController.ts
@@ -1,41 +1,70 @@
 import { Context, Status, STATUS_TEXT } from "https://deno.land/x/oak/mod.ts";
 
 import { Response } from "../helper/repsonse.ts";
-
 import { saveUser, selectUserByPhone } from "../repository/userRepo.ts";
-import { encryptPass } from "../security/pass.ts";
+import { encryptPass, verifyPass } from "../security/pass.ts";
 
 export const signInHandler = async (context: Context) => {
-  context.response.status = Status.OK;
-  context.response.body = "SignIn";
+  const body = context.request.body();
+  const reqData = await body.value;
+
+  const user = await selectUserByPhone(reqData.phone);
+
+  if (!user) {
+    return Response(
+      context,
+      Status.NotFound,
+      { status: Status.NotFound, message: STATUS_TEXT.get(Status.NotFound) },
+    );
+  }
+
+  const passIsValid = verifyPass(reqData.password, user.password);
+  if (!passIsValid) {
+    return Response(
+      context,
+      Status.Unauthorized,
+      {
+        status: Status.Unauthorized,
+        message: STATUS_TEXT.get(Status.Unauthorized),
+      },
+    );
+  }
+  return Response(
+    context,
+    Status.OK,
+    {
+      status: Status.OK,
+      message: STATUS_TEXT.get(Status.OK),
+    },
+  );
 };
 
 export const signUpHandler = async (context: Context) => {
   const body = context.request.body();
-  const reqData = body.value;
-  console.log("requ", context);
-  // const user = await selectUserByPhone(reqData.phone);
-  // if (user) {
-  //   return Response(
-  //     context,
-  //     Status.Conflict,
-  //     { status: Status.Conflict, message: STATUS_TEXT.get(Status.Conflict) },
-  //   );
-  // }
+  const reqData = await body.value;
+  const user = await selectUserByPhone(reqData.phone);
 
-  // reqData.password = encryptPass(reqData.password);
-  // const insertId = await saveUser(body.value);
+  if (user) {
+    return Response(
+      context,
+      Status.Conflict,
+      { status: Status.Conflict, message: STATUS_TEXT.get(Status.Conflict) },
+    );
+  }
 
-  // if (!insertId) {
-  //   return Response(
-  //     context,
-  //     Status.InternalServerError,
-  //     {
-  //       status: Status.InternalServerError,
-  //       message: STATUS_TEXT.get(Status.InternalServerError),
-  //     },
-  //   );
-  // }
+  reqData.password = encryptPass(reqData.password);
+  const insertId = await saveUser(reqData);
+
+  if (!insertId) {
+    return Response(
+      context,
+      Status.InternalServerError,
+      {
+        status: Status.InternalServerError,
+        message: STATUS_TEXT.get(Status.InternalServerError),
+      },
+    );
+  }
   return Response(
     context,
     Status.OK,
--- repository/userRepo.ts
+++ repository/userRepo.ts
@@ -1,7 +1,7 @@
 import DB from "../db/database.ts";
 import type { User } from "../model/user.ts";
 
-const userCollection = DB.collection("users");
+const userCollection = DB.collection<User>("users");
 
 export const saveUser = async (user: User) => {
   return await userCollection.insertOne(user);
